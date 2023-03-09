a8f5125e445f080c8dab9126c3fa8efb8d61b894
abhi-procreator <hi2abhi.shrm@gmail.com>
2023-03-10 00:09:45 +0530

    initial updated

--- src/app/import-section/import-section.component.ts
+++ src/app/import-section/import-section.component.ts
@@ -12,12 +12,24 @@ export class ImportSectionComponent {
   exportFile: boolean = false;
   icons: boolean = false;
 
+  Screen1: boolean = true;
+  Screen2: boolean = false;
+  Screen3: boolean = false;
+  Screen4: boolean = false;
+  Screen5: boolean = false;
+
   uploadFile(event: any) {
     this.fileUpload = event.target.files[0].name;
-    this.selected = true;
     this.icons = true;
+    this.Screen1 = false;
+    this.Screen2 = true;
     setTimeout(() => {
       this.exportFile = true;
     }, 1000);
   }
+
+  secondShow() {
+    this.Screen2 = false;
+    this.Screen3 = true;
+   }
 }
