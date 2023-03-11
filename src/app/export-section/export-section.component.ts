31f69d00b5e0d9e20bd2c1bb2572c6cdfe15df63
abhi-procreator <hi2abhi.shrm@gmail.com>
2023-03-11 16:09:25 +0530

    initial commit

---
+++ src/app/export-section/export-section.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-export-section',
+  templateUrl: './export-section.component.html',
+  styleUrls: ['./export-section.component.css']
+})
+export class ExportSectionComponent {
+
+  Screen1: boolean = true;
+  Screen2: boolean = false;
+  Screen3: boolean = false;
+  selected: boolean = false;
+  exportFile: boolean = true;
+  constructor() { }
+
+  uploadFile(event: any) { }
+}
